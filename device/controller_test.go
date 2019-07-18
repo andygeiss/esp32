c22385f47ac100693e095990b61819cd48b06b27
andygeiss <4ndygeiss@gmail.com>
2019-07-18 20:45:10 +0200

    modules updated

--- device/controller_test.go
+++ device/controller_test.go
@@ -2,7 +2,6 @@ package device_test
 
 import (
 	"github.com/andygeiss/assert"
-	"github.com/andygeiss/assert/is"
 	"github.com/andygeiss/esp32/device"
 	"testing"
 )
@@ -10,12 +9,12 @@ import (
 func TestControllerSetupErrorShouldBeNil(t *testing.T) {
 	ctrl := device.NewController()
 	err := ctrl.Setup()
-	assert.That(t, err, is.Equal(nil))
+	assert.That(t, err, nil)
 }
 
 func TestControllerLoopErrorShouldBeNil(t *testing.T) {
 	ctrl := device.NewController()
 	ctrl.Setup()
 	err := ctrl.Loop()
-	assert.That(t, err, is.Equal(nil))
+	assert.That(t, err, nil)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/andygeiss/esp32
 go 1.12
 
 require (
-	github.com/andygeiss/assert v0.0.6
-	github.com/andygeiss/esp32-controller v0.0.1
+	github.com/andygeiss/assert v0.0.7
+	github.com/andygeiss/esp32-controller v0.0.2
 )
