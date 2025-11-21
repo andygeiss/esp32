e29f2f224061fb09128b28b67949a9c72c239e09
Andy <4ndygeiss@gmail.com>
2025-11-21 08:02:10 +0100

    test: switch to cloud native utils

--- device/controller_test.go
+++ device/controller_test.go
@@ -3,19 +3,19 @@ package device_test
 import (
 	"testing"
 
+	"github.com/andygeiss/cloud-native-utils/assert"
 	"github.com/andygeiss/esp32/device"
-	"github.com/andygeiss/utils/assert"
 )
 
 func TestControllerSetupErrorShouldBeNil(t *testing.T) {
 	ctrl := device.NewController()
 	err := ctrl.Setup()
-	assert.That("setup should return without an error", t, err, nil)
+	assert.That(t, "setup should return without an error", err, nil)
 }
 
 func TestControllerLoopErrorShouldBeNil(t *testing.T) {
 	ctrl := device.NewController()
 	ctrl.Setup()
 	err := ctrl.Loop()
-	assert.That("loop once should return without an error", t, err, nil)
+	assert.That(t, "loop once should return without an error", err, nil)
 }
--- go.mod
+++ go.mod
@@ -1,8 +1,9 @@
 module github.com/andygeiss/esp32
 
-go 1.18
+go 1.24.6
 
 require (
-	github.com/andygeiss/esp32-controller v0.1.0
+	github.com/andygeiss/cloud-native-utils v0.2.12
+	github.com/andygeiss/esp32-controller v0.1.1
 	github.com/andygeiss/utils v0.9.1
 )
